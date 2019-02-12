bf742509c6948daa4c4c087766df6fe8247ef199
kow1000 <kow1000@gmail.com>
2019-02-12 09:35:04 +0900

    DTO 만드는중..

---
+++ src/main/project/dto/TBoard.java
@@ -0,0 +1,26 @@
+package dto;
+
+import java.util.Date;
+
+public class TBoard {
+  
+	private int TNUM;
+	private String TKIND;
+	private String TSUBJECT;
+	private String TCONTENT;
+	private String TMETHOD;
+	private String TSITUATION;
+	private Date TREG_DATE;
+	private String TBOOK_NAME;
+	private String TBOOK_ORI_IMG_NAME;
+	private String TBOOK_STORE_IMG_NAME;
+	private int TPRICE;
+	private String TBOOK_STATUS;
+	private String TBOOK_GENRE;
+	private Date TSTART_DATE;
+	private String TDELIVERY_NUM;
+	private String TDELIVERY_NAME;
+	private Date TDELIVERY_NUM_DATE;
+	private int TCHARGE;
+	private String TBUYERADDR;
+}
