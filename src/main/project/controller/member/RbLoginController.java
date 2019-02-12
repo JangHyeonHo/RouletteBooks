5ec51bc5ded056ce987f7b209bc7e36160480fa6
gusghwkd202 <gusghwkd202@naver.com>
2019-02-12 09:34:00 +0900

    com

--- src/main/project/controller/member/RbLoginController.java
+++ src/main/project/controller/member/RbLoginController.java
@@ -1,14 +1,21 @@
 package controller.member;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import command.LoginCommand;
 import controller.FrontControllerInterface;
+import service.member.MemberLoginService;
 @Controller
 @RequestMapping("/member/login")
 public class RbLoginController implements FrontControllerInterface {
+	
+	@Autowired
+	MemberLoginService service;
+	
 
 	// 권도완
 	@Override
@@ -25,9 +32,11 @@ public class RbLoginController implements FrontControllerInterface {
 		return null;
 	}
 	
-	public String OpenProcessPost(LoginCommand command) {
+	@RequestMapping(method = RequestMethod.POST)
+	public String OpenProcessPost(@ModelAttribute LoginCommand command) {
 		// TODO Auto-generated method stub
-		return null;
+		
+		return "redirect:/main";
 	}
 
 }
