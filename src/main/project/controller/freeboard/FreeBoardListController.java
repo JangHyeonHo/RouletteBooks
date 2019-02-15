5612519c0383a3d541b5028ba54524979139ecd6
kwondowan <kwondowan1@gmail.com>
2019-02-15 17:46:42 +0900

    kdw

--- src/main/project/controller/freeboard/FreeBoardListController.java
+++ src/main/project/controller/freeboard/FreeBoardListController.java
@@ -17,36 +17,36 @@ import service.freeboard.FreeBoardListService;
 @RequestMapping("/freeboardlist")
 public class FreeBoardListController implements FrontControllerInterface {
 
-	//권도완
-	private FreeBoardListService freeboardlistservice;
-	
-	@Autowired
-	public FreeBoardListController(FreeBoardListService freeboardlistservice) {
-		this.freeboardlistservice = freeboardlistservice;
-	}
-	
-	@RequestMapping(method = RequestMethod.GET)
-	public String OpenProcessGet(Model model) {
-		// TODO Auto-generated method stub
-		System.out.println("자유게시판 오픈");
-		List<FreeBoard> Freeboardlist = freeboardlistservice.freeboardlist();
-		model.addAttribute("Freeboardlist",Freeboardlist);
-		return "freeboard/FreeBoard";
-	}
-	
+   //권도완
+   private FreeBoardListService freeboardlistservice;
+   
+   @Autowired
+   public FreeBoardListController(FreeBoardListService freeboardlistservice) {
+      this.freeboardlistservice = freeboardlistservice;
+   }
+   
+   @RequestMapping(method = RequestMethod.GET)
+   public String OpenProcessGet(Model model) {
+      // TODO Auto-generated method stub
+      System.out.println("자유게시판 오픈");
+      List<FreeBoard> Freeboardlist = freeboardlistservice.freeboardlist();
+      model.addAttribute("Freeboardlist",Freeboardlist);
+      return "freeboard/FreeBoard";
+   }
+   
 
-	@Override
-	public String OpenProcessPost() {
-		// TODO Auto-generated method stub
-		return null;
-	}
+   @Override
+   public String OpenProcessPost() {
+      // TODO Auto-generated method stub
+      return null;
+   }
 
-	@Override
-	public String OpenProcessGet() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-	
+   @Override
+   public String OpenProcessGet() {
+      // TODO Auto-generated method stub
+      return null;
+   }
+   
 
 
 }
