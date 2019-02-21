752960870b5b920c0c71f8de7e86ea61d90fe30d
gusghwkd202 <gusghwkd202@naver.com>
2019-02-21 10:04:34 +0900

    commit

--- src/main/project/dao/BookListDao.java
+++ src/main/project/dao/BookListDao.java
@@ -1,13 +1,17 @@
 package dao;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.List;
 
 import javax.sql.DataSource;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 
 import command.BookContractMoneyCommand;
+import command.BookCountCommand;
 import command.PublisherContractCommand;
 
 public class BookListDao {
@@ -28,6 +32,21 @@ public class BookListDao {
 			jdbcTemplate.update(sql, command.getConsNum(), command.getConsName(),book.getBookName(), book.getBookContractMoney(), book.getBookMoney());
 		}
 		
+	}
+
+	public List<BookCountCommand> getPubBookCount() {
+		// TODO Auto-generated method stub
+		sql = "select bpublisher_num, count(*) as cnt from bookList bl join CONTRACT_PUBLISHER on(CB_PUBLISHER_NO = bpublisher_num) GROUP BY bpublisher_num";
+		return jdbcTemplate.query(sql, new RowMapper<BookCountCommand>() {
+
+			@Override
+			public BookCountCommand mapRow(ResultSet rs, int rowNum) throws SQLException {
+				// TODO Auto-generated method stub
+				return new BookCountCommand().setPubNum(rs.getString("bpublisher_num"))
+						.setBookCount(rs.getInt("cnt"));
+			}
+			
+		});
 	}
 	
 	
